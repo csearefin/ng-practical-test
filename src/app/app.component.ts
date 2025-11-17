bd69ec04eceec5d1d77bb83bf3f43bacf57a8e5b
Sagir Hossain <sagir@impelitsolutions.com>
2025-11-18 00:36:14 +0600

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  menus = signal([
+    { title: 'Characters', url: '/characters' },
+    { title: 'Boxes', url: '/boxes' },
+  ]);
+
   constructor() {
     setTheme('bs5');
   }
